f64024e535e4dcc76c7d8c805fd3087d3f6b7b9a
jaoo10 <j.marcus.faria@gmail.com>
2014-03-21 15:36:12 -0300

    BFS pronto

--- src/python/main.py
+++ src/python/main.py
@@ -1,10 +1,68 @@
 import sys
+ 
+
+def abreArquivo():
+	global vertices
+	global arestas
+	arquivo = open(sys.argv[1],"r")
+	arq = arquivo.readlines()
+	arq = [(a.strip()) for a in arq]
+	indice = arq.index("#")
+	vertices = arq[:indice]
+	arestas = arq[indice+1:]
+	arestas = [(e.split(' ')) for e in arestas]
+
+def buildListaAdj():
+	global listaAdj
+	listaAdj = {}
+	for v in vertices:
+		listaAdj[v] = []
+	for a in arestas:
+		listaAdj[a[0]].append(a[1])
+
+def buildListaPesos():
+	global listaPesos
+	listaPesos = {}
+	for a in arestas:
+		listaPesos[(a[0],a[1])] = a[2]
+	
+def bfs():
+	listaCores = []
+	listaDist = []
+	for v in vertices:
+		listaCores.append('branco')
+		listaDist.append(0)
+	listaCores[0] = 'cinza'
+	listaDist[0] = 0
+	fila = []
+	fila.append(vertices[0])
+	while fila != []:
+		primeiro = fila.pop(0)
+		for v in listaAdj[primeiro]:
+			if listaCores[vertices.index(v)] == 'branco':
+				listaDist[vertices.index(v)] = listaDist[vertices.index(primeiro)] + 1
+				listaCores[vertices.index(v)] = 'cinza'
+				fila.append(v)
+		listaCores[vertices.index(primeiro)] = 'preto'
+	printBFS(listaDist)
+
+def printBFS(distancias):
+	for v in vertices:
+		print vertices[0] + ' ' + v + ' ' + str(distancias[vertices.index(v)])
+
 
 def main():
-    print "parametros:",
-    for s in sys.argv[1:]:
-        print s,
-    print
+	print "parametros:"
+	for s in sys.argv[1:]:
+		print s
+	print
+	abreArquivo()
+	buildListaAdj()
+	bfs()
+
 
 if __name__ == "__main__":
-    main()
+	main()
+
+
+
